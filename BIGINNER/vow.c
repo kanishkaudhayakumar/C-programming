85e3902b544af55af37e0a43f8caed0adf8d83f9
kanishkaudhayakumar <38652872+kanishkaudhayakumar@users.noreply.github.com>
2018-04-24 10:01:24 +0000

    Add files via upload

---
+++ BIGINNER/vow.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	char n;
+	scanf("%s",&n);
+	if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u')
+	{
+		printf("Vowel");
+	}
+	else
+	{
+		printf("Constant");
+	}
+return 0;
+}
