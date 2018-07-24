e22536aaf17adc9d5d95bda5587331204d04d234
kanishkaudhayakumar <38652872+kanishkaudhayakumar@users.noreply.github.com>
2018-07-24 10:52:56 +0000

    Update pal.c

--- BIGINNER/pal.c
+++ BIGINNER/pal.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 int main()
 {
-    int n, reversedInteger = 0, remainder, originalInteger;
+    int N, reversedInteger = 0, remainder, originalInteger;
 
    
-    scanf("%d", &n);
+    scanf("%d", &N);
 
-    originalInteger = n;
+    originalInteger = N;
 
     // reversed integer is stored in variable 
-    while( n!=0 )
+    while( N>=1000)
     {
-        remainder = n%10;
+        remainder = N%10;
         reversedInteger = reversedInteger*10 + remainder;
-        n=n/10;
+        N=N/10;
     }
 
     // palindrome if orignalInteger and reversedInteger are equal
