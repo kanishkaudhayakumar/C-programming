78130c2dbcaefe4dd66e6f9d22fa4ee562726140
kanishkaudhayakumar <38652872+kanishkaudhayakumar@users.noreply.github.com>
2018-07-31 10:10:37 +0000

    Create primec.c

---
+++ BIGINNER/primec.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int N, i, flag = 0;
+
+ 
+    scanf("%d",&N);
+	if(N<=1000)
+	{
+  	  	for(i=2; i<=N/2; ++i)
+    		{
+
+       			 if(N%i==0)
+       			 {
+            flag=1;
+            break;
+        }
+    }
+}
+
+    if (flag==0)
+        printf("Yes");
+    else
+        printf("NO");
+    
+    return 0;
+}
