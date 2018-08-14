59ee56fda8635009179787410ca85bc15bee5d57
kanishkaudhayakumar <38652872+kanishkaudhayakumar@users.noreply.github.com>
2018-08-14 11:05:32 +0000

    Create five.c

---
+++ BIGINNER/five.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+        int sum=0,i,n;
+     
+     
+        scanf("%ld",&n);
+        for(i=1;i<=n;i++)
+        {
+            printf("\n 5 * %ld = %ld",i,5*i);
+        }
+        
+       
+}
